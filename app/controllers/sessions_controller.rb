0fe527055447e0c36c4ece26cc4f28773c192642
Tomas Valent <equivalent@eq8.eu>
2018-03-08 22:37:28 +0100

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    require 'irb'; binding.irb
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
