5ba0a5ff3faa7102ec15c09e1adfec1b42f5dc15
Tomas Valent <equivalent@eq8.eu>
2018-03-08 23:17:22 +0100

    noscope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ require 'omniauth'
 require 'omniauth-github'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
