bbdca824e8b345ab63eebf28cdc580ff1d4ce275
Tomas Valent <equivalent@eq8.eu>
2018-03-08 22:55:04 +0100

    up

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+require 'omniauth'
+require 'omniauth-github'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
